82e4b2d7052d3d05ad0cbeac721ac18a95cf6dcc
jameshegarty1 <jhegartyodowd@gmail.com>
2023-11-14 23:48:05 +0000

    Serializer tests

--- django_backend/api/serializers.py
+++ django_backend/api/serializers.py
@@ -7,18 +7,26 @@ class TagSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class HabitRecordSerializer(serializers.ModelSerializer):
-    habitId = serializers.IntegerField(write_only=True)
+    habit = serializers.PrimaryKeyRelatedField(queryset=Habit.objects.all())
     class Meta:
         model = HabitRecord
-        fields = ['habitId', 'notes']
-    
-    
-
+        fields = ['habit', 'notes']
 
 
 class HabitSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, required=False)
+
 
     class Meta:
         model = Habit
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'tags', 'habit_type', 'goal_quantity', 'current_quantity', 'start_date', 'end_date', 'notification_time', 'priority', 'parent_habit']
+
+    def update(self, instance, validated_data):
+        tags_data = validated_data.pop('tags', None)
+        instance = super().update(instance, validated_data)
+
+        if tags_data is not None:
+            tags = [Tag.objects.get_or_create(name=tag_data['name'])[0] for tag_data in tags_data]
+            instance.tags.set(tags)
+        
+        return instance
--- django_backend/tests/api/test_serializers.py
+++ django_backend/tests/api/test_serializers.py
@@ -1,98 +1,140 @@
-'''
-from django.test import TestCase
-from api.models import Habit, HabitRecord, Tag
-from api.serializers import TagSerializer, HabitRecordSerializer, HabitSerializer
-from collections import OrderedDict
+from api.serializers import HabitSerializer, HabitRecordSerializer, TagSerializer
+from datetime import date, time, timedelta
+import pytest
 
-class TagSerializerTestCase(TestCase):
-    def test_tag_serializer(self):
-        tag = Tag.objects.create(name="Example")
-        serializer = TagSerializer(tag)
-        self.assertEqual(serializer.data, {'id': tag.id, 'name': "Example", 'description': None})
+pytestmark = pytest.mark.django_db
+class TestHabitSerializer:
+    def test_serialize_model_instance(self, habit_factory):
+        habit = habit_factory()
+        serializer = HabitSerializer(instance=habit)
 
-    def test_invalid_tag_name(self):
-        """Test validation for invalid tag name."""
-        data = {"name": ""}  # empty name
-        serializer = TagSerializer(data=data)
-        self.assertFalse(serializer.is_valid())
-        self.assertIn("name", serializer.errors)
+        assert serializer.data['name'] == habit.name
+        assert serializer.data['description'] == habit.description
 
-class HabitRecordSerializerTestCase(TestCase):
-    def setUp(self):
-        self.habit = Habit.objects.create(name="Read a book")
+    def test_serialize_with_tags(self, habit_factory, tag_factory):
+        tag1 = tag_factory()
+        tag2 = tag_factory()
+        habit = habit_factory()
+        habit.tags.set([tag1, tag2])
+        serializer = HabitSerializer(instance=habit)
+
+        assert len(serializer.data['tags']) == 2
+        assert serializer.data['tags'][0]['name'] == tag1.name
+        assert serializer.data['tags'][1]['name'] == tag2.name
+
+    def test_validation(self, habit_factory):
+        habit = habit_factory()
+        data = {'name': '', 'description': habit.description}  # Invalid data
+        serializer = HabitSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert 'name' in serializer.errors
+
+    def test_serialize_with_optional_fields(self, habit_factory):
+        habit = habit_factory(start_date=date.today(), end_date=date.today() + timedelta(days=1))
+        serializer = HabitSerializer(instance=habit)
+
+        assert 'start_date' in serializer.data
+        assert 'end_date' in serializer.data
+        assert serializer.data['start_date'] == habit.start_date.isoformat()
+        assert serializer.data['end_date'] == habit.end_date.isoformat()
+
+    def test_update_with_tags(self, habit_factory, tag_factory):
+        habit = habit_factory()
+        tag1 = tag_factory()
+        tag2 = tag_factory()
+
+        update_data = {
+            'name': 'Updated Habit',
+            'description': 'Updated Description',
+            'tags': [{'name': tag1.name}, {'name': tag2.name}]
+            }
         
-    def test_habit_record_serializer(self):
-        #ipdb.set_trace()
-        print(self.habit.id)
-        data = {
-            "habit": self.habit.id,
-            "notes": "Read a chapter of 'Django for beginners'."
-        }
-        serializer = HabitRecordSerializer(data=data)
+        serializer = HabitSerializer(habit, data=update_data)
+    
         if not serializer.is_valid():
             print(serializer.errors)
-        self.assertTrue(serializer.is_valid())
-        serializer.save()
-        self.assertEqual(HabitRecord.objects.first().notes, "Read a chapter of 'Django for beginners'.")
+        assert serializer.is_valid()
+        updated_habit = serializer.save()
+        assert updated_habit.name == 'Updated Habit'
+        assert updated_habit.description == 'Updated Description'
+        assert list(updated_habit.tags.values_list('name', flat=True)) == [tag1.name, tag2.name]
 
-    def test_missing_habit_id(self):
-        """Test validation for missing habitId."""
-        data = {"notes": "Read a chapter."}
-        serializer = HabitRecordSerializer(data=data)
-        self.assertFalse(serializer.is_valid())
-        self.assertIn("habit", serializer.errors)
+    def test_update_without_tags(self, habit_factory):
+        habit = habit_factory()
 
-    def test_invalid_habit_id(self):
-        """Test validation for invalid habitId."""
-        data = {"habitId": 9999, "notes": "Read a chapter."}  # Assuming no Habit with id 9999
-        serializer = HabitRecordSerializer(data=data)
-        self.assertFalse(serializer.is_valid())
-        self.assertIn("habit", serializer.errors)
-
-class HabitSerializerTestCase(TestCase):
-    def setUp(self):
-        self.tag1 = Tag.objects.create(name="Morning")
-        self.tag2 = Tag.objects.create(name="Evening")
-        self.habit = Habit.objects.create(name="Workout", description="Daily workout session")
-        self.habit.tags.set([self.tag1, self.tag2])
-        
-    def test_habit_serializer(self):
-        serializer = HabitSerializer(self.habit)
-        expected_data = {
-            'id': self.habit.id,
-            'name': 'Workout',
-            'description': 'Daily workout session',
-            'tags': [
-                OrderedDict([('id', 4), ('name', 'Morning'), ('description', None)]), 
-                OrderedDict([('id', 5), ('name', 'Evening'), ('description', None)])
-            ], 
-            'habit_type': 'Infinite',
-            'goal_quantity': None,
-            'current_quantity': 0,
-            'start_date': None,
-            'end_date': None,
-            'notification_time': None,
-            'priority': 'None',
-            'parent_habit': None
+        update_data = {
+            'name': 'Updated Habit', 
+            'description': 'Updated Description'
         }
-        print(serializer.data)
-        print(expected_data)
-        self.assertEqual(serializer.data, expected_data)
 
-    def test_invalid_tags(self):
-        """Test validation for invalid tags."""
-        habit_data = {
-            "name": "Test habit",
-            "tags": [{"name": ""}]  # Invalid tag with empty name
-        }
-        serializer = HabitSerializer(data=habit_data)
-        self.assertFalse(serializer.is_valid())
-        self.assertIn("tags", serializer.errors)
+        serializer = HabitSerializer(instance=habit, data=update_data)
+        if not serializer.is_valid():
+            print(serializer.errors)
+        assert serializer.is_valid()
+        updated_habit = serializer.save()
+        assert updated_habit.name == 'Updated Habit'
+        assert updated_habit.description == 'Updated Description'
+    
 
-    def test_missing_name(self):
-        """Test validation for missing habit name."""
-        habit_data = {}
-        serializer = HabitSerializer(data=habit_data)
-        self.assertFalse(serializer.is_valid())
-        self.assertIn("name", serializer.errors)
-'''
+
+class TestHabitRecordSerializer:
+    def test_serialize_model_instance(self, habit_record_factory):
+        habit_record = habit_record_factory()
+        serializer = HabitRecordSerializer(instance=habit_record)
+
+        assert serializer.data['habit'] == habit_record.habit.id
+        assert serializer.data['notes'] == habit_record.notes
+        # ... assert other fields ...
+
+    def test_validation(self, habit_record_factory):
+        habit_record = habit_record_factory()
+        data = {'habit': None, 'notes': habit_record.notes}  # Invalid data
+        serializer = HabitRecordSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert 'habit' in serializer.errors
+
+    def test_invalid_habit(self):
+        invalid_data = {'habit': 9999, 'notes': 'Invalid habit id'}
+        serializer = HabitRecordSerializer(data=invalid_data)
+
+        assert not serializer.is_valid()
+        assert 'habit' in serializer.errors
+
+    def test_update_habit_record(self, habit_record_factory):
+        habit_record = habit_record_factory()
+        update_data = {'notes': 'Updated notes'}
+        serializer = HabitRecordSerializer(instance=habit_record, data=update_data, partial=True)
+
+        if not serializer.is_valid():
+            print(serializer.errors)
+        assert serializer.is_valid()
+        updated_record = serializer.save()
+        assert updated_record.notes == 'Updated notes'
+
+
+class TestTagSerializer:
+    def test_serialize_model_instance(self, tag_factory):
+        tag = tag_factory()
+        serializer = TagSerializer(instance=tag)
+
+        assert serializer.data['name'] == tag.name
+        assert serializer.data['description'] == tag.description
+
+    def test_validation(self, tag_factory):
+        data = {'name': ''}  # Invalid data
+        serializer = TagSerializer(data=data)
+
+        assert not serializer.is_valid()
+        assert 'name' in serializer.errors
+
+    def test_update_tag(self, tag_factory):
+        tag = tag_factory()
+        update_data = {'name': 'Updated Tag', 'description': 'Updated Description'}
+        serializer = TagSerializer(instance=tag, data=update_data)
+
+        assert serializer.is_valid()
+        updated_tag = serializer.save()
+        assert updated_tag.name == 'Updated Tag'
+        assert updated_tag.description == 'Updated Description'
